f646ebcca86906089986e259b1f82987a41ee5eb
ASquirrelsTail <>
2020-04-26 15:12:04 +0100

    Set port to environment variable.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 8080;
 const http = require('http').createServer(app);
 
 const io = require('socket.io')(http);
